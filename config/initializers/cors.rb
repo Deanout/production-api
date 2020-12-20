fc423542a517b21d94aaed10ed9b2c1cd9e437d1
Dean DeHart <DeHartDean@gmail.com>
2020-12-20 00:55:15 -0500

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '47.26.3.24'
+    origins '47.26.3.24:8080'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
