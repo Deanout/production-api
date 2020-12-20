83d06dd9c73444981f752a6fc8102dbf14bdf816
Dean DeHart <DeHartDean@gmail.com>
2020-12-20 01:19:46 -0500

    Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://172.31.54.169:8080/'
+    origins '47.26.3.24'
+    origins '47.26.3.24:8080'
 
     resource '*',
              headers: :any,
