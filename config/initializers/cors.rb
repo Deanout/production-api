10c4dcb1a01ba93c3cbfbb64592f9719848f92ed
Dean DeHart <DeHartDean@gmail.com>
2020-12-21 04:19:12 -0500

    Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://47.26.3.24:8080'
+    origins 'http://localhost:8080'
 
     resource '/api/v1/*',
              headers: :any,
---
+++ frontend/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    devServer: {
+      port: 8080,
+      host: '47.26.3.24'
+    }
+  }
